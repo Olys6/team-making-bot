6e64a7139aa9750ba578acf6e0fb903678819d3f
Olys6 <oliver@skjevesland.com>
2022-05-10 12:26:56 +0200

    able to make teams of three and fixed bugs

--- team_generator.py
+++ team_generator.py
@@ -22,10 +22,13 @@ async def on_message(message):
 
     msg = message.content
 
-    if msg.startswith('!teamup'):
+    if msg.startswith('!teamup') and (msg.split("!teamup")[1].startswith(" ") or msg.split("!teamup")[1].startswith("2")):
         team1 = []
         team2 = []
-        msg_content_no_cmd = msg.split("!teamup ")[1]
+        if(msg.split("!teamup")[1].startswith(" ")):
+            msg_content_no_cmd = msg.split("!teamup ")[1]
+        elif(msg.split("!teamup")[1].startswith("2")):
+            msg_content_no_cmd = msg.split("!teamup2 ")[1]
         if "," in msg: 
             split_names = msg_content_no_cmd.split(", ")
         else: 
@@ -35,7 +38,7 @@ async def on_message(message):
                 team1.append(name)
             else:
                 team2.append(name)
-        for teammate in team1: 
+        for teammate in split_names: 
             if(len(team1) > len(team2) + 1): 
                 random_pop = random.randint(0, len(team1) - 1)
                 team2.append(team1[random_pop])
@@ -45,8 +48,56 @@ async def on_message(message):
                 team1.append(team2[random_pop])
                 team2.pop(random_pop)
 
+
         await message.channel.send(f"team 1: {team1}")
         await message.channel.send(f"team 2: {team2}")
+
+    if msg.startswith('!teamup') and msg.split("!teamup")[1].startswith("3"):
+        team1 = []
+        team2 = []
+        team3 = []
+        msg_content_no_cmd = msg.split("!teamup3 ")[1]
+        if "," in msg:
+            split_names = msg_content_no_cmd.split(", ")
+        else:
+            split_names = msg_content_no_cmd.split(" ")
+        for name in split_names:
+            randomNum = random.randint(0, 2)
+            if randomNum == 0:
+                team1.append(name)
+            elif randomNum == 1:
+                team2.append(name)
+            else:
+                team3.append(name)
+        for teammate in split_names:
+            if(len(team1) > len(team2) + 1):
+                random_pop = random.randint(0, len(team1) - 1)
+                team2.append(team1[random_pop])
+                team1.pop(random_pop)
+            elif len(team1) > len(team3) + 1:
+                random_pop = random.randint(0, len(team1) - 1)
+                team3.append(team1[random_pop])
+                team1.pop(random_pop)
+            elif len(team2) > len(team3) +1:
+                random_pop = random.randint(0, len(team3) - 1)
+                team2.append(team3[random_pop])
+                team3.pop(random_pop)
+            elif(len(team2) > len(team1) + 1):
+                random_pop = random.randint(0, len(team2) - 1)
+                team1.append(team2[random_pop])
+                team2.pop(random_pop)
+            elif len(team3) > len(team1) + 1:
+                random_pop = random.randint(0, len(team3) - 1)
+                team1.append(team3[random_pop])
+                team3.pop(random_pop)
+            elif len(team3) > len(team2) + 1:
+                random_pop = random.randint(0, len(team3) - 1)
+                team2.append(team3[random_pop])
+                team3.pop(random_pop)
+
+        await message.channel.send(f"team 1: {team1}")
+        await message.channel.send(f"team 2: {team2}")
+        await message.channel.send(f"team 3: {team3}")
 
 
 
